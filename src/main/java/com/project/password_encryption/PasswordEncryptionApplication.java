f9b898143111a9e4c92b3c0bc92515ecddeb1c8e
Oliver Castillo <olcastillot@gmail.com>
2023-12-05 12:21:02 -0500

    Add base files

---
+++ src/main/java/com/project/password_encryption/PasswordEncryptionApplication.java
@@ -0,0 +1,13 @@
+package com.project.password_encryption;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordEncryptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasswordEncryptionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/password_encryption/PasswordEncryptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.password_encryption;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasswordEncryptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
