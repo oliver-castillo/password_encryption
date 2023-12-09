b4c8b59617e0805785e628fa1f9a078355aeb16a
Oliver Castillo <olcastillot@gmail.com>
2023-12-08 22:30:59 -0500

    Modify service

--- src/main/java/com/project/password_encryption/services/IPasswordEncryptionService.java
+++ src/main/java/com/project/password_encryption/services/IPasswordEncryptionService.java
@@ -3,13 +3,13 @@ package com.project.password_encryption.services;
 import java.util.Map;
 
 public interface IPasswordEncryptionService {
+
   public String encodeWithBcrypt(String password);
 
   public String encodeWithScrypt(String password);
 
   public String encodeWithPbkdf2(String password);
 
-  public String encodeWithAlgorithm(String password, String algorithm);
+  public String encodeWithArgon2id(Map<String, Object> params);
 
-  public String encodeWithArgon2id(String password, Map<String, Integer> params);
 }
--- src/main/java/com/project/password_encryption/services/PasswordEncryptionService.java
+++ src/main/java/com/project/password_encryption/services/PasswordEncryptionService.java
@@ -2,15 +2,15 @@ package com.project.password_encryption.services;
 
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Logger;
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import com.project.password_encryption.exceptions.custom.BadRequestException;
 
 import de.mkammerer.argon2.Argon2;
 import de.mkammerer.argon2.Argon2Factory;
@@ -22,6 +22,8 @@ public class PasswordEncryptionService implements IPasswordEncryptionService {
   BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
   SCryptPasswordEncoder sCryptPasswordEncoder = new SCryptPasswordEncoder();
   Pbkdf2PasswordEncoder pbkdf2PasswordEncoder = new Pbkdf2PasswordEncoder();
+
+  Logger logger = Logger.getLogger(PasswordEncryptionService.class.getName());
 
   @Override
   public String encodeWithBcrypt(String password) {
@@ -39,33 +41,34 @@ public class PasswordEncryptionService implements IPasswordEncryptionService {
   }
 
   @Override
-  public String encodeWithAlgorithm(String password, String algorithm) {
-    try {
-      MessageDigest messageDigest = MessageDigest.getInstance(algorithm);
-      byte[] hashedBytes = messageDigest.digest(password.getBytes(StandardCharsets.UTF_8));
-      StringBuilder hexStringBuilder = new StringBuilder();
-      for (byte b : hashedBytes) {
-        hexStringBuilder.append(String.format("%02x", b));
-      }
-      return hexStringBuilder.toString();
-    } catch (NoSuchAlgorithmException e) {
-      e.printStackTrace();
-      return null;
-    }
-  }
-
-  @Override
-  public String encodeWithArgon2id(String password, Map<String, Integer> params) {
-    char[] passwordCharArray = password.toCharArray();
+  public String encodeWithArgon2id(Map<String, Object> params) {
+    checkParams(params);
+    char[] passwordCharArray = params.get("password").toString().toCharArray();
     Charset charset = StandardCharsets.UTF_8;
     String hashedPassword = argon2.hash(
-        params.get("iterations"),
-        params.get("memory"),
-        params.get("parallelism"),
+        (Integer) params.get("iterations"),
+        (Integer) params.get("memory"),
+        (Integer) params.get("parallelism"),
         passwordCharArray,
         charset);
     argon2.wipeArray(passwordCharArray);
-    System.out.println(params.get("memory"));
     return hashedPassword;
   }
+
+  public void checkParams(Map<String, Object> params) throws BadRequestException {
+    int iterations = (Integer) params.get("iterations");
+    int memory = (Integer) params.get("memory");
+    int parallelism = (Integer) params.get("parallelism");
+    if (iterations <= 0 || iterations > 20) {
+      throw new BadRequestException("The number of iterations must be greater than 0 and less than or equal to 20");
+    }
+    if (memory <= 0 || memory > 100000) {
+      throw new BadRequestException(
+          "The number of kilobytes of memory must be greater than 0 and less than or equal to 100000");
+    }
+    if (parallelism <= 0 || parallelism > 10) {
+      throw new BadRequestException("The parallelism number must be greater than 0 and less than or equal to 10");
+    }
+  }
+
 }
