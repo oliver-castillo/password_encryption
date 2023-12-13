88d68df8077661a963bb4a229ed603d87a712b10
Oliver Castillo <olcastillot@gmail.com>
2023-12-12 19:03:57 -0500

    Update controller

--- src/main/java/com/project/password_encryption/controller/PasswordEncryptionController.java
+++ src/main/java/com/project/password_encryption/controller/PasswordEncryptionController.java
@@ -1,6 +1,5 @@
 package com.project.password_encryption.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
